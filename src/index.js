06c3977d1f7fb75cc3e187200f6ced38e826cc4f
Victor <mr.dyrban@gmail.com>
2021-08-10 12:57:50 +0300

    separated the app

--- src/index.js
+++ src/index.js
@@ -1,41 +1,26 @@
 import {createStore} from 'redux';
+import reducer from './reducer';
+import {inc, dec,rnd,res} from './actions';
 
-const reducer = (state = 0 , action) => {
-    switch (action.type) {
-        case 'INC':
-            return state + 1;
-        case 'DEC':
-            return state - 1;
-        case 'RES':
-            return state = 0;
-        case 'RND':
-            return state + action.value;
-        default:
-            return state;
-    }
-};
-
-const inc = () =>({type: 'INC'});
-const dec = () =>({type: 'DEC'});
-const res = () =>({type: 'RES'});
-const rnd = (value) =>({type: 'RND',value});
 const store = createStore(reducer);
 
+const {dispatch} = store;
+
 document.getElementById('inc').addEventListener('click', () =>{
-    store.dispatch(inc());
+    dispatch(inc());
 });
 
 document.getElementById('dec').addEventListener('click', () =>{
-    store.dispatch(dec());
+    dispatch(dec());
 });
 
 document.getElementById('res').addEventListener('click', () =>{
-    store.dispatch(res());
+    dispatch(res());
 });
 
 document.getElementById('rnd').addEventListener('click', () =>{
     let value = Math.floor(Math.random() * 10);
-    store.dispatch(rnd(value));
+    dispatch(rnd(value));
 });
 
 const update = () =>{
