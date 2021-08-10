8f84207df414e6178523847bc56a0d00033bc464
Victor <mr.dyrban@gmail.com>
2021-08-10 13:24:42 +0300

    ref code with redux featchers

--- src/index.js
+++ src/index.js
@@ -1,24 +1,21 @@
 import {createStore, bindActionCreators} from 'redux';
 import reducer from './reducer';
-import {inc, dec,rnd,res} from './actions';
+import * as actions from './actions';
 
 const store = createStore(reducer);
 const {dispatch} = store;
 
-const incDispatch = bindActionCreators(inc, dispatch);
-const decDispatch = bindActionCreators(dec, dispatch);
-const resDispatch = bindActionCreators(res, dispatch);
-const rndDispatch = bindActionCreators(rnd, dispatch);
+const {inc, dec, rnd, res} = bindActionCreators(actions, dispatch);
 
-document.getElementById('inc').addEventListener('click', incDispatch);
+document.getElementById('inc').addEventListener('click', inc);
 
-document.getElementById('dec').addEventListener('click',decDispatch);
+document.getElementById('dec').addEventListener('click',dec);
 
-document.getElementById('res').addEventListener('click',resDispatch);
+document.getElementById('res').addEventListener('click',res);
 
 document.getElementById('rnd').addEventListener('click', () =>{
     let value = Math.floor(Math.random() * 10);
-    rndDispatch(value)
+    rnd(value)
 });
 
 const update = () =>{
